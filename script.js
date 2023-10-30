1c088b0ba0845c8d0fe23da4530385cf67990d98
Glitch (glitch-hello-website) <none>
2023-10-30 00:19:43 +0000

    🏩😚 Checkpoint ./top-secret.html:1848311/992 ./.glitch-assets:1848311/440 ./style.css:1848311/494 ./script.js:1848311/314

--- script.js
+++ script.js
@@ -42,3 +42,13 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+
+function zoom(e){
+  var zoomer = e.currentTarget;
+  e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
+  e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
+  x = offsetX/zoomer.offsetWidth*100
+  y = offsetY/zoomer.offsetHeight*100
+  zoomer.style.backgroundPosition = x + '% ' + y + '%';
+}
